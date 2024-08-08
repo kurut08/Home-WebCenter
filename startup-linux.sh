510e2490406ba4e8037274eae9a5502951392fea
Kurut08 <Kurut08@gmail.com>
2024-08-08 14:19:08 +0200

    Changed name of frontend container. Updates startup script.

--- startup-linux.sh
+++ startup-linux.sh
@@ -3,14 +3,14 @@ git pull
 sudo docker stop docker-backend
 sudo docker stop FileBrowser
 sudo docker stop pgAdmin
-sudo docker stop webcenter-front
+sudo docker stop webcenter-frontend
 
 sudo docker container rm docker-backend
 sudo docker container rm FileBrowser
 sudo docker container rm --volumes pgAdmin
-sudo docker container rm --volumes webcenter-front
+sudo docker container rm --volumes webcenter-frontend
 
 sudo docker image rm home_webcenter-docker-backend:latest
-sudo docker image rm home_webcenter-front:latest
+sudo docker image rm home_webcenter-frontend:latest
 
 cd docker && sudo docker compose up --detach
