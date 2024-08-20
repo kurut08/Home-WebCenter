90c5649142f8dd11272209804146b3343c907fdb
Kurut08 <Kurut08@gmail.com>
2024-08-20 11:58:26 +0200

    Update to the docker-compose.yml and startup-linux.sh - frontend container got removed and future os-status container got included

--- startup-linux.sh
+++ startup-linux.sh
@@ -2,15 +2,18 @@
 git pull
 sudo docker stop docker-backend
 sudo docker stop FileBrowser
+sudo docker stop os-status
 sudo docker stop pgAdmin
 sudo docker stop webcenter-frontend
 
 sudo docker container rm docker-backend
 sudo docker container rm FileBrowser
+sudo docker container rm os-status
 sudo docker container rm --volumes pgAdmin
-sudo docker container rm --volumes webcenter-frontend
+sudo docker container rm webcenter-frontend
 
 sudo docker image rm home_webcenter-docker-backend:latest
-sudo docker image rm home_webcenter-frontend:latest
+sudo docker image rm home_webcenter-os-status:latest
+sudo docker image rm home_webcenter-webcenter-frontend:latest
 
 cd docker && sudo docker compose up --detach
