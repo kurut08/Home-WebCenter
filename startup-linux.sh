c065d317af954863cb2a0c8c39b7b8640d912f9b
Kurut08 <Kurut08@gmail.com>
2024-08-23 22:44:00 +0200

    Updated startup-linux.sh to include onedev and messages about current operation.

--- startup-linux.sh
+++ startup-linux.sh
@@ -1,21 +1,36 @@
 #! /bin/bash
+echo ------------Updating local repository------------
 git pull
+echo ------------------------------------------------------------------------
+
+echo -e "\n\n\n"
+
+echo ------------Stopping running docker containers------------
 sudo docker stop docker-backend
 sudo docker stop FileBrowser
 sudo docker stop onedev
 sudo docker stop os-status
 sudo docker stop pgAdmin
 sudo docker stop webcenter-frontend
+echo ------------------------------------------------------------------------
 
+echo -e "\n\n\n"
+
+echo ------------Removing docker containers------------
 sudo docker container rm docker-backend
 sudo docker container rm FileBrowser
 sudo docker container rm os-status
 sudo docker container rm onedev
 sudo docker container rm --volumes pgAdmin
 sudo docker container rm webcenter-frontend
+echo ------------------------------------------------------------------------
 
+echo -e "\n\n\n"
+
+echo ------------Removing images of local available containers------------
 sudo docker image rm home_webcenter-docker-backend:latest
 sudo docker image rm home_webcenter-os-status:latest
 sudo docker image rm home_webcenter-webcenter-frontend:latest
+echo ------------------------------------------------------------------------
 
 cd docker && sudo docker compose up --detach
